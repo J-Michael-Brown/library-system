8910da07f89a545989ac89bebf4d5229de859bc0
Michael Brown and Quinn Miller <quinn@>
2018-12-05 16:32:19 -0800

    backup our database

---
+++ database_backup.sql
@@ -0,0 +1,235 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.4
+-- Dumped by pg_dump version 10.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: books; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.books (
+    id integer NOT NULL,
+    author character varying,
+    title character varying
+);
+
+
+ALTER TABLE public.books OWNER TO "Guest";
+
+--
+-- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.books_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.books_id_seq OWNER TO "Guest";
+
+--
+-- Name: books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
+
+
+--
+-- Name: checkouts; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.checkouts (
+    id integer NOT NULL,
+    book_id integer,
+    patron_id integer,
+    date character varying
+);
+
+
+ALTER TABLE public.checkouts OWNER TO "Guest";
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.checkouts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.checkouts_id_seq OWNER TO "Guest";
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.checkouts_id_seq OWNED BY public.checkouts.id;
+
+
+--
+-- Name: patrons; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.patrons (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE public.patrons OWNER TO "Guest";
+
+--
+-- Name: patron_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.patron_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.patron_id_seq OWNER TO "Guest";
+
+--
+-- Name: patron_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.patron_id_seq OWNED BY public.patrons.id;
+
+
+--
+-- Name: books id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
+
+
+--
+-- Name: checkouts id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.checkouts ALTER COLUMN id SET DEFAULT nextval('public.checkouts_id_seq'::regclass);
+
+
+--
+-- Name: patrons id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.patrons ALTER COLUMN id SET DEFAULT nextval('public.patron_id_seq'::regclass);
+
+
+--
+-- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.books (id, author, title) FROM stdin;
+1	Miller, Quinn	My Thesaurus
+\.
+
+
+--
+-- Data for Name: checkouts; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.checkouts (id, book_id, patron_id, date) FROM stdin;
+\.
+
+
+--
+-- Data for Name: patrons; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.patrons (id, name) FROM stdin;
+\.
+
+
+--
+-- Name: books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.books_id_seq', 1, true);
+
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.checkouts_id_seq', 1, false);
+
+
+--
+-- Name: patron_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.patron_id_seq', 1, false);
+
+
+--
+-- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: checkouts checkouts_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.checkouts
+    ADD CONSTRAINT checkouts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: patrons patron_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.patrons
+    ADD CONSTRAINT patron_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
